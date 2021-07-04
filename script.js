27f046abb05b04a3fa84442d3d53bb6c6ff7f7f8
Norin Oul <norin@Norins-MacBook-Pro.local>
2021-07-04 16:21:31 +0700

    update dark theme and details

--- script.js
+++ script.js
@@ -1,52 +1,87 @@
-const moonPath =
-  "M18 27.5C18 42.6878 27.5 55 27.5 55C12.3122 55 0 42.6878 0 27.5C0 12.3122 12.3122 0 27.5 0C27.5 0 18 12.3122 18 27.5Z";
-const circlePath =
-  "M55 27.5C55 42.6878 42.6878 55 27.5 55C12.3122 55 0 42.6878 0 27.5C0 12.3122 12.3122 0 27.5 0C42.6878 0 55 12.3122 55 27.5Z";
-
-const darkMode = document.querySelector("#darkMode");
-let toggleStatus = false;
-
-darkMode.addEventListener("click", () => {
-  const timeline = anime.timeline({
-    duration: 750,
-    easing: "easeOutExpo"
-  });
-  morphTo(timeline, toggleStatus);
-  toggleStatus = !toggleStatus;
+$(document).ready(function() {
+  $("#color_mode").on("change", function () {
+      colorModePreview(this);
+  })
 });
 
-function morphTo(timeline, toggler) {
-  const ModeText = document.getElementById("ModeText");
-  if (ModeText.innerHTML === "Dark Theme") {
-    ModeText.innerHTML = "Light Theme";
-  } else {
-    ModeText.innerHTML = "Dark Theme";
+function colorModePreview(ele) {
+  if($(ele).prop("checked") == true){
+      $('body').addClass('dark-preview');
+      $('body').removeClass('white-preview');
   }
-  ModeText.classList.toggle("DarkMode");
-  timeline
-    .add({
-      targets: ".circle",
-      d: [{ value: toggler ? circlePath : moonPath }]
-    })
-    .add(
-      {
-        targets: "#darkMode",
-        rotate: toggler ? 40 : 320
-      },
-      "-=700"
-    )
-    .add(
-      {
-        targets: ".scene",
-        backgroundColor: toggler ? "#f1f1f1" : "#333"
-      },
-      "-=700"
-    );
-};
-function changeDarkModeText(darkMode){
-  const DarkModeText = "Turn to Dark Mode";
-  if(darkMode){
-    DarkModeText = "Turn to Light Mode"
+  else if($(ele).prop("checked") == false){
+      $('body').addClass('white-preview');
+      $('body').removeClass('dark-preview');
   }
-  return DarkModeText
 }
+anime({
+  targets: '.aboutme-image',
+  translateX: 700,
+  translateY: 40,
+  duration: 1800,
+  easing: 'easeInOutSine'
+});
+anime({
+  targets: '.aboutme-text',
+  translateX: 100,
+  translateY: 40,
+  direction: 'reverse',
+  duration: 1800,
+  easing: 'easeInOutSine'
+});
+anime({
+  targets: '.education-text',
+  translateX: 500,
+  translateY: 40,
+  duration: 1800,
+  easing: 'easeInOutSine'
+});
+anime({
+  targets: '.education-image',
+  translateX: 700,
+  translateY: 60,
+  direction: 'reverse',
+  duration: 1800,
+  easing: 'easeInOutSine'
+});
+anime({
+  targets: '.funfacts-text',
+  translateX: 100,
+  translateY: 40,
+  duration: 1800,
+  easing: 'easeInOutSine'
+});
+anime({
+  targets: '.funfacts-bar',
+  translateX: 200,
+  duration: 1800,
+  easing: 'easeInOutSine'
+});
+anime({
+  targets: '#guitarist-bar',
+  width: '200%', // -> from '28px' to '100%',
+  easing: 'easeInOutQuad',
+  direction: 'alternate',
+  loop: true
+});
+anime({
+  targets: '#gamer-bar',
+  width: '350%', // -> from '28px' to '100%',
+  easing: 'easeInOutQuad',
+  direction: 'alternate',
+  loop: true
+});
+anime({
+  targets: '.contact',
+  translateX: 800,
+  translateY: -350,
+  duration: 1800,
+  easing: 'easeInOutSine'
+});
+anime({
+  targets: '.myresume',
+  translateX: 800,
+  translateY: -350,
+  duration: 1800,
+  easing: 'easeInOutSine'
+});
